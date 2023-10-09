8c73b3560c57065c3aeadf58a4a30ac0b1a046a0
Eoin Elliott <43847943+eoinell@users.noreply.github.com>
2023-10-09 12:06:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ with cols[1]:
 cols = st.columns(3)
 
 def rolled():
-    vals = [randint(1, side),  sign* randint(1, s) for s, sign in zip(additional_sides, dice_signs), modifier]
+    vals = [randint(1, side),  *[sign* randint(1, s) for s, sign in zip(additional_sides, dice_signs)], modifier]
     val = sum(vals)
     with cols[1]:
         st.subheader(f"{'+'.join(vals}" = {val})
